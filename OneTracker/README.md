2528fee7fdd86ba3ee9da4934fa1d1462cbc758c
shivamfw10 <akgeeta8874@gmail.com>
2022-07-21 19:58:14 +0530

    updated

--- OneTracker/README.md
+++ OneTracker/README.md
@@ -1,4 +1,4 @@
-# OneTracker
+# MSPP2OneTrackerUI
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
 
--- OneTracker/karma.conf.js
+++ OneTracker/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/one-tracker'),
+      dir: require('path').join(__dirname, './coverage/ms-pp2-one-tracker-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
