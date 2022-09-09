c12423e9216a048385e68388a72bd7609b199066
shivamfw10 <akgeeta8874@gmail.com>
2022-09-09 18:04:37 +0530

    Loop

---
+++ C_Sharp_Training/LoopsInCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopsInCSharp {
+
+    class Program {
+        static void Main(string[] args) {
+
+            //Loop
+
+            //While Loop
+            int i = 0;
+            while (i < 5) { 
+                Console.WriteLine(i);
+                i++;
+            }
+
+            //Do While
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 5);
+        } 
+    }
+
+}
