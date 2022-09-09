6285bf6a65c5d26909398c418e0f3d6eac7c12e3
shivamfw10 <akgeeta8874@gmail.com>
2022-09-09 17:54:32 +0530

    Methods3

---
+++ C_Sharp_Training/c_sharp_method/Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_method
+{
+    class Hello
+    {
+        public string name = "Shivam";
+        public int health = 49;
+    };
+}
---
+++ C_Sharp_Training/c_sharp_method/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace c_sharp_method {
+    class Program {
+        static void Main(string[] args)
+        {
+            Greet("Shivam");
+            Hello hello = new Hello();
+            Console.WriteLine(hello.name);
+            Console.WriteLine(Average(10, 15, 25));
+            Console.ReadLine();
+        }
+        static void Greet(string name)
+        {
+            Console.WriteLine("Good Morning"+name);
+        }
+
+        private static float Average(int a, int b, int c)
+        {
+            float sum = a + b + c;
+            return sum/3;
+        }
+    }
+}
