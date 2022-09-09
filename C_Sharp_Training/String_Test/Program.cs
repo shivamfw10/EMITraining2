58da9330d385b6cd2fb83fb00b33bf073f484e2d
shivamfw10 <akgeeta8874@gmail.com>
2022-09-09 18:11:52 +0530

    String Practice3.

---
+++ C_Sharp_Training/String_Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace String_Test 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string Hello = "I'm Shivam";
+            Console.WriteLine(Hello.Length);
+            Console.WriteLine(Hello.ToUpper());
+            Console.WriteLine(Hello.ToLower());
+            Console.WriteLine(string.Concat(Hello," ","from Kanpur"));
+            */
+
+            //String Interpolation with example
+
+            /*
+            Console.WriteLine("Welcome ! Enter Your Name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Quantity :");
+            string quantity = Console.ReadLine();
+            Console.WriteLine($"Hi! {name}. You got {quantity} candies.");//String Interpolation
+            */
+
+            //Escape Sequence Character
+            Console.WriteLine("Welcome! \n May I Help You");//For New Line \n
+            Console.WriteLine("Welcome! \t May I Help You");//For One Tab Space \n
+            Console.WriteLine("Welcome! \" May I Help You");
+
+
+
+
+        }
+    }
+}
