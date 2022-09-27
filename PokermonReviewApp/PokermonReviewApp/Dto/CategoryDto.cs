3899d810363d40ca566d0b8e72c7596c50610659
shivamfw10 <akgeeta8874@gmail.com>
2022-09-27 16:22:30 +0530

    Dto

---
+++ PokermonReviewApp/PokermonReviewApp/Dto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PokermonReviewApp.Dto
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PokermonReviewApp/PokermonReviewApp/Dto/ReviewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PokermonReviewApp.Dto
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Rating { get; set; }
+    }
+}
